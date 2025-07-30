911f95c2f21695bf12f98534ab2a1b25e3a7f130
Veranika <veranikadarmawan402@gmail.com>
2025-07-30 13:46:50 +0800

    feat:create products API

--- src/app.js
+++ src/app.js
@@ -2,9 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const userRoutes = require('./routes/userRoutes');
+const productRoutes = require('./routes/productRoutes');
 app.use(express.json()); // Middleware untuk parsing JSON body
 app.use('/api/users', userRoutes);
-
+app.use('/api/products', productRoutes);
 
 // Rute untuk setiap modul
 
